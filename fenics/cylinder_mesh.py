9b06508d606380c66ed0744e8948544112f73f50
Trung Pham <pbtrung@outlook.com>
2024-07-01 11:59:16 +0100

    Update

--- fenics/cylinder_mesh.py
+++ fenics/cylinder_mesh.py
@@ -7,62 +7,54 @@ from mpi4py import MPI
 from vars import *
 
 
-def gen_cylinder_gmsh(fname):
+def gen_cylinder_gmsh(fname_noext, mesh_size_max):
     gmsh.initialize()
 
-    gmsh.model.add("cylinder")
-    # x, y, z, dx, dy, dz, r
     cy = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, l, r)
-    ci = gmsh.model.occ.addBox(0, 0, 0, 1, 1, 1)
     gmsh.model.occ.synchronize()
-    gmsh.option.setNumber("Mesh.MeshSizeMax", size_max)
-    gmsh.option.setNumber("Mesh.MshFileVersion", 2.2)
-    gmsh.model.addPhysicalGroup(gdim, [cy], 1)
-    gmsh.model.addPhysicalGroup(gdim, [ci], 2)
-    gmsh.model.setPhysicalName(gdim, 1, "m1")
-    gmsh.model.setPhysicalName(gdim, 2, "m2")
-    gmsh.model.mesh.generate(gdim)
 
-    gmsh.write(fname + ".msh")
-    # gmsh.fltk.run()
+    # smallest 0.005
+    # small    0.01
+    # med      0.03
+    # big      0.05
+    gmsh.option.setNumber("Mesh.MeshSizeMax", mesh_size_max)
+    gmsh.option.setNumber("Mesh.MshFileVersion", 2.2)
+    gmsh.model.addPhysicalGroup(dim3, [cy], 1)
+    gmsh.model.setPhysicalName(dim3, 1, "m1")
+    gmsh.model.mesh.generate(dim3)
+
+    gmsh.write(fname_noext + ".msh")
     gmsh.finalize()
 
 
-def gen_cylinder_xdmf(fname, fname_phys, physical_group):
-    if not physical_group:
-        mesh, cell_markers, facet_markers = gmshio.read_from_msh(
-            fname + ".msh", MPI.COMM_WORLD, 0, gdim=gdim
+def gen_cylinder_xdmf(fname_noext):
+    mesh, cell_markers, facet_markers = gmshio.read_from_msh(
+        fname_noext + ".msh", MPI.COMM_WORLD, 0, gdim=dim3
+    )
+    mesh.name = "mesh"
+    cell_markers.name = f"{mesh.name}_cells"
+    facet_markers.name = f"{mesh.name}_facets"
+    with XDMFFile(mesh.comm, fname_noext + ".xdmf", "w") as file:
+        mesh.topology.create_connectivity(2, 3)
+        file.write_mesh(msh)
+        file.write_meshtags(
+            cell_markers,
+            mesh.geometry,
+            geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{mesh.name}']/Geometry",
         )
-
-        with XDMFFile(mesh.comm, fname + ".xdmf", "w") as xdmf:
-            xdmf.write_mesh(mesh)
-            print("done!")
-    else:
-        msh, ct, ft = gmshio.read_from_msh(fname_phys + ".msh", MPI.COMM_WORLD, 0, gdim=gdim)
-        msh.name = "mesh"
-        ct.name = f"{msh.name}_cells"
-        ft.name = f"{msh.name}_facets"
-        print(ft.find(1))
-        print(ct.find(2))
-        with XDMFFile(msh.comm, fname_phys + ".xdmf", "w") as file:
-            msh.topology.create_connectivity(2, 3)
-            file.write_mesh(msh)
-            file.write_meshtags(
-                ct, msh.geometry, geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry"
-            )
-            file.write_meshtags(
-                ft, msh.geometry, geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{msh.name}']/Geometry"
-            )
-            print("done!")
+        file.write_meshtags(
+            facet_markers,
+            mesh.geometry,
+            geometry_xpath=f"/Xdmf/Domain/Grid[@Name='{mesh.name}']/Geometry",
+        )
+        print("done!")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        if sys.argv[3] != "phys":
-            gen_cylinder_gmsh(sys.argv[1])
-            gen_cylinder_xdmf(sys.argv[1], sys.argv[2], False)
-        else:
-            gen_cylinder_gmsh(sys.argv[1])
-            gen_cylinder_xdmf(sys.argv[1], sys.argv[2], True)
+    if len(sys.argv) == 3:
+        fname_noext = sys.argv[1]
+        mesh_size_max = float(sys.argv[2])
+        gen_cylinder_gmsh(fname_noext, mesh_size_max)
+        gen_cylinder_xdmf(fname_noext)
     else:
         print("Must have filename.")
--- fenics/vars.py
+++ fenics/vars.py
@@ -1,8 +1,4 @@
-gdim = 3
+dim3 = 3
+dim2 = 2
 r = 1.0
 l = 2.0
-
-# small 0.01
-# med   0.03
-# big   0.05
-size_max = 0.05
