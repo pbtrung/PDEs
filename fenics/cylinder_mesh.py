0ffb35c84fc8adb230b44a7253b3a1da1ca8daf7
Trung Pham <pbtrung@outlook.com>
2024-07-01 18:12:22 +0100

    Update

--- fenics/cylinder_mesh.py
+++ fenics/cylinder_mesh.py
@@ -12,7 +12,6 @@ def gen_cylinder_gmsh(fname_noext, mesh_size_max):
 
     cy1 = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, l, r)
     cy2 = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, l, rtop)
-    cy3 = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, l, r)
     gmsh.model.occ.cut([(3, cy1)], [(3, cy2)], removeObject=True, removeTool=False)
 
     # p100 = gmsh.model.occ.addPoint(0, 0, l)
@@ -56,8 +55,10 @@ def gen_cylinder_gmsh(fname_noext, mesh_size_max):
     # gmsh.model.addPhysicalGroup(2, other_boundary_entities, tag=1)
     # gmsh.model.setPhysicalName(dim=dim2, tag=1, name="boundary")
 
-    gmsh.model.addPhysicalGroup(dim3, [cy3], tag=1)
-    gmsh.model.setPhysicalName(dim=dim3, tag=1, name="cylinder")
+    gmsh.model.addPhysicalGroup(dim3, [cy1], tag=1)
+    gmsh.model.setPhysicalName(dim=dim3, tag=1, name="cy1")
+    gmsh.model.addPhysicalGroup(dim3, [cy2], tag=2)
+    gmsh.model.setPhysicalName(dim=dim3, tag=2, name="cy2")
 
     gmsh.model.addPhysicalGroup(dim2, [6, 9], tag=1)
     gmsh.model.setPhysicalName(dim=dim2, tag=1, name="bottom")
