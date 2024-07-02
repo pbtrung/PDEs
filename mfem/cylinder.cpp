d7b9b4168caf947d4d63bd87120ff83b76278260
Trung Pham <pbtrung@outlook.com>
2024-07-02 15:19:43 +0100

    Update

--- mfem/cylinder.cpp
+++ mfem/cylinder.cpp
@@ -140,7 +140,7 @@ int main(int argc, char *argv[]) {
     VectorConstantCoefficient vCoeff(v);
     ConstantCoefficient dCoeff(d);
 
-    ConvectionDiffusionOperator oper(fespace, vCoeff, dCoeff, ess_tdof_list);
+    ConvectionDiffusionOperator oper(fespace, vCoeff, dCoeff);
     BackwardEulerSolver ode_solver;
     ode_solver.Init(oper);
 
