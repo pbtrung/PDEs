838a58345830f4cd5bafc765e6ad4a5c9f4f6cbb
Trung Pham <pbtrung@outlook.com>
2024-07-02 17:46:41 +0100

    Update

--- mfem/cylinder.cpp
+++ mfem/cylinder.cpp
@@ -127,7 +127,7 @@ int main(int argc, char *argv[]) {
 
     fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
     ConstantCoefficient one(1.0);
-    c.ProjectCoefficient(one, ess_tdof_list);
+    c.ProjectBdrCoefficient(one, ess_tdof_list);
 
     Vector v(3);
     v = 0.0;
