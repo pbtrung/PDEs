5b109c8d44773a726460c5a026e5125c249d5372
Trung Pham <pbtrung@outlook.com>
2024-07-04 14:37:34 +0100

    Update

--- mfem/cylinder.cpp
+++ mfem/cylinder.cpp
@@ -10,7 +10,7 @@ class ConvectionDiffusionOperator : public TimeDependentOperator {
     ParFiniteElementSpace &fespace;
     ParBilinearForm *M;
     ParBilinearForm *K;
-    HypreParMatrix *Mmat, *Kmat;
+    HypreParMatrix Mmat, Kmat;
     Array<int> ess_tdof_list;
     double c0 = 1.0;
 
@@ -30,10 +30,10 @@ class ConvectionDiffusionOperator : public TimeDependentOperator {
         K->AddDomainIntegrator(new ConvectionIntegrator(vCoeff));
         K->AddDomainIntegrator(new DiffusionIntegrator(dCoeff));
         M->Assemble(0);
-        M->FormSystemMatrix(ess_tdof_list, *Mmat);
+        M->FormSystemMatrix(ess_tdof_list, Mmat);
         // M->Finalize();
         K->Assemble(0);
-        K->FormSystemMatrix(ess_tdof_list, *Kmat);
+        K->FormSystemMatrix(ess_tdof_list, Kmat);
         // K->Finalize();
 
         // Mmat = M->ParallelAssemble();
@@ -53,11 +53,11 @@ class ConvectionDiffusionOperator : public TimeDependentOperator {
     }
 
     virtual void ImplicitSolve(const double dt, const Vector &x, Vector &y) {
-        HypreParMatrix A(*Mmat);
-        A.Add(dt, *Kmat);
+        HypreParMatrix A(Mmat);
+        A.Add(dt, Kmat);
         cg.SetOperator(A);
         Vector B(x.Size());
-        Mmat->Mult(x, B);
+        Mmat.Mult(x, B);
         cg.Mult(B, y);
         // y.SetSubVector(ess_tdof_list, c0);
     }
@@ -65,8 +65,8 @@ class ConvectionDiffusionOperator : public TimeDependentOperator {
     virtual ~ConvectionDiffusionOperator() {
         delete M;
         delete K;
-        delete Mmat;
-        delete Kmat;
+        // delete Mmat;
+        // delete Kmat;
     }
 };
 
